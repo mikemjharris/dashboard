3560f5d36b287e1f0784cc3ad831507d518491cd
Mike Harris <hello@mikemjharris.com>
2018-11-14 12:45:42 +0000

    call off to api gateway

--- src/index.js
+++ src/index.js
@@ -2,38 +2,21 @@ import './main.scss';
 import axios from 'axios';
 
 var app = new Vue({
-	el: '#app',
-	data: {
-		projects: {
-			website: {
-				status: {},
-				url: "https://www.mikemjharris.com",
-				versionEndpoint: "/version.json"
-
-			},
-
-			blog: {
-				status: {},
-				url: "https://blog.mikemjharris.com",
-				versionEndpoint: "/version.json"
-			}
-		}
-	},
-	methods: {
-		loadData: function () {
-			for (var project_name in this.projects) {
-				let project = this.projects[project_name];
-				Promise.all([axios.get(project.url + project.versionEndpoint), project_name])
-				.then((response)  =>  {
-					this.projects[response[1]].status = response[0].data;
-				}, (error)  =>  {
-					this.projects[response[1]].error = true;
-				})
-			}
-		}
-	},
-	beforeMount() {
-		this.loadData();
-	}
+  el: '#app',
+  data: {
+    projects: []
+  },
+  methods: {
+    loadData: function () {
+      axios.get('https://sasptp6a7h.execute-api.eu-west-1.amazonaws.com/default/nodeServerStatus')
+      .then((response) => {
+        console.log(response);
+        this.projects = response.data;
+      })
+    }
+  },
+  beforeMount() {
+    this.loadData();
+  }
 });
 
