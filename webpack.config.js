f653eff241636f6b08cee950d5ef7a794c36247f
Mike Harris <hello@mikemjharris.com>
2018-11-15 09:26:25 +0000

    clean dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -4,27 +4,27 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   watch: true,
   mode: 'development',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	},
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
 
 
-	module: {
-		rules: [{
-			test: /\.scss$/,
-			use: [
-				MiniCssExtractPlugin.loader,
-				{
-					loader: "css-loader"
-				}, {
-					loader: "sass-loader"
-				}]
-		}]
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "./style.css"
-		})
-	]
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader"
+        }]
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./style.css"
+    })
+  ]
 };
--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,34 +1,38 @@
 const webpack = require('webpack');
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-		entry: './src/index.js',
+    entry: './src/index.js',
   },
 
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, '../dist')
-	},
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist')
+  },
 
 
-	module: {
-		rules: [{
-			test: /\.scss$/,
-			use: [
-				MiniCssExtractPlugin.loader,
-				{
-					loader: "css-loader"
-				}, {
-					loader: "sass-loader"
-				}]
-		}]
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "./style.css"
-		}),
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader"
+        }]
+    }]
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {
+      root: path.resolve(__dirname, '../')
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./style.css"
+    }),
     new webpack.EnvironmentPlugin([
       'NODE_ENV',
     ]),
